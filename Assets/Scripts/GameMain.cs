1995f3fc216c92ea241c002bcde3cd404a698c99
mob-kayokomiyoshi <kayoko.miyoshi@mobcast.jp>
2015-09-04 12:15:10 +0900

    Stateにfinishを追加

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -9,7 +9,8 @@ public class GameMain : MonoBehaviour {
 		PlayerAttack,
 		EnemyAttack,
 		Playerwin,
-		Playerlose
+		Playerlose,
+		finish
 	};
 
 	State state = State.Wait;
@@ -27,68 +28,71 @@ public class GameMain : MonoBehaviour {
 
 	void Update()
 	{
-		switch (state)
-		{
-			case State.Wait:
-				if (Input.GetMouseButtonDown(0))
-				{
-					state = State.PlayerAttack;
-				}
-				break;
-			case State.PlayerAttack:
-				foreach(Player player in playerlist){
-					foreach(Enemy enemy in enemylist){
-						if(enemy.HP>0){	//enemyのHPが0以上かどうか
-							player.playerAttack(enemy);
-							break;
-						}
+		switch (state) {
+		case State.Wait:
+			if (Input.GetMouseButtonDown (0)) {
+				state = State.PlayerAttack;
+			}
+			break;
+
+		case State.PlayerAttack:
+			foreach (Player player in playerlist) {
+				foreach (Enemy enemy in enemylist) {
+					if (enemy.HP > 0) {	//enemyが生きているかどうか
+						player.playerAttack (enemy);
+						break;
 					}
 				}
-			bool win=true;
-			foreach(Enemy enemy in enemylist){
-				if(enemy.HP>0){
-					win=false;
+			}
+			bool win = true;
+			foreach (Enemy enemy in enemylist) {
+				if (enemy.HP > 0) {
+					win = false;
 					break;
 				}
 			}
-			if(win){
-				state=State.Playerwin;
-			}
-			else{
+			if (win) {
+				state = State.Playerwin;
+			} else {
 				state = State.EnemyAttack;
 			}
-				break;
-			case State.EnemyAttack:
-				foreach(Enemy enemy in enemylist){
-					foreach(Player player in playerlist){
-						if(player.HP>0){
-							enemy.enemyAttack(player);
-							break;
-						}
+			break;
+
+		case State.EnemyAttack:
+			foreach (Enemy enemy in enemylist) {
+				foreach (Player player in playerlist) {
+					if (player.HP > 0) {
+						enemy.enemyAttack (player);
+						break;
 					}
 				}
+			}
 			bool lose = true;
-			foreach(Player player in playerlist){
-				if(player.HP>0){
-					lose=true;
+			foreach (Player player in playerlist) {
+				if (player.HP > 0) {
+					lose = false;
 					break;
 				}
 			}
-			if(lose){
-				state=State.Playerlose;
+			if (lose) {
+				state = State.Playerlose;
+			} else {
+				state = State.Wait;
 			}
-			else{
-				state=State.Wait;
-			}
-			state = State.Wait;
-				break;
-			case State.Playerwin:
-					Debug.Log("=== Player Win! ===");
-				break;
-			case State.Playerlose:
-					Debug.Log("=== Player Lose... ===");
-				break;
-			}
+			break;
 
+		case State.Playerwin:
+			Debug.Log ("=== Player Win! ===");
+			state = State.finish;
+			break;
+
+		case State.Playerlose:
+			Debug.Log ("=== Player Lose... ===");
+			state = State.finish;
+			break;
+
+		case State.finish:
+			break;
 		}
 	}
+}
