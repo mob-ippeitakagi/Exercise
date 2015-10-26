f6c4afe3dd3119b1abf453ebd9af2e0966581432
mob-kayokomiyoshi <kayoko.miyoshi@mobcast.jp>
2015-10-26 18:12:09 +0900

    属性判定のメソッド化

--- Assets/Scripts/Charactor.cs
+++ Assets/Scripts/Charactor.cs
@@ -7,6 +7,7 @@ class Charactor
 	public int HP;
 	public int AttackPower;
 	public string Attribute;
+	public float rate;
 
 	public Charactor(string name,int hp,int attack,string attribute){
 		NAME = name;
@@ -19,34 +20,38 @@ class Charactor
 	{
 		int damage = UnityEngine.Random.Range (1, AttackPower + 1);
 		Debug.Log ("=== " + NAME + " Attack! ===");
-		if (Attribute == "red") {
-			if (charactor.Attribute == "green") {
-				damage *= 2;
-				Debug.Log("ダメージ2倍");
-			} else if (charactor.Attribute == "blue") {
-				damage /= 2;
-				Debug.Log("ダメージ半減");
-			}
-		} else if (Attribute == "blue") {
-			if (charactor.Attribute == "red") {
-				damage *= 2;
-				Debug.Log("ダメージ2倍");
-			} else if (charactor.Attribute == "green") {
-				damage /= 2;
-				Debug.Log("ダメージ半減");
-			}
-		} else if (Attribute == "green") {
-			if (charactor.Attribute == "blue") {
-				damage *= 2;
-				Debug.Log("ダメージ2倍");
-			} else if (charactor.Attribute == "red") {
-				damage /= 2;
-				Debug.Log("ダメージ半減");
-			}
+
+		Hantei (charactor);
+		damage = (int)(Mathf.Max(damage,1) * rate);
+		if (rate == 2f) {
+			Debug.Log ("ダメージ2倍");
+		} else if (rate == 0.5f) {
+			Debug.Log ("ダメージ半減");
 		}
-		damage = Mathf.Max(damage,1);
 		charactor.HP -= damage;
 		Debug.Log ("Damage: " + damage);
 		Debug.Log (charactor.NAME + "HP: " + charactor.HP);
+	}
+	float Hantei(Charactor charactor){
+		if (Attribute == "red") {
+			if (charactor.Attribute == "green") {
+				rate = 2f;
+			} else if (charactor.Attribute == "blue") {
+				rate = 0.5f;
+			}
+		} else if (Attribute == "blue") {
+			if (charactor.Attribute == "red") {
+				rate = 2f;
+			} else if (charactor.Attribute == "green") {
+				rate = 0.5f;
+			}
+		} else if (Attribute == "green") {
+			if (charactor.Attribute == "blue") {
+				rate = 2f;
+			} else if (charactor.Attribute == "red") {
+				rate = 0.5f;
+			}
+		}
+		return rate;
 	}
 }
