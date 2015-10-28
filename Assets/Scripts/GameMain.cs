956b0e2c8d50ed16a283f08bdc8e628bde2b4e6d
mob-ippeitakagi <ippei.takagi@mobcast.jp>
2015-10-28 11:20:27 +0900

    Revert "PlayerとEnemyをスクリプト化しリストを作成"

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -1,97 +1,82 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class GameMain : MonoBehaviour {
+	int playerHP = 150;
+	int playerAttackPower = 15;
+	int enemyHP = 100;
+	int enemyAttackPower = 10;
+	bool finishedFlg = false;
 
 	enum State{
 		Wait,
 		PlayerAttack,
 		EnemyAttack,
-		Playerwin,
-		Playerlose,
-		finish
+		Finish
 	};
 
-	int turn=1;
 	State state = State.Wait;
-	List<Player> playerlist =new List<Player>();	//Playerクラスのリストを作成しplayerlistに格納
-	List<Enemy> enemylist =new List<Enemy>();
-
-	void Awake(){
-		this.playerlist.Add (new Player ("player1",100,15,"red"));		//playerlistにplayer情報を代入
-		this.playerlist.Add (new Player ("player2",120,10,"blue"));
-		this.enemylist.Add (new Enemy ("enemy1",50,10,"green",1));
-		this.enemylist.Add (new Enemy ("enemy2",70,7,"red",2));
-	}
-
 	void Update()
 	{
-		switch (state) {
+		switch (state)
+		{
 			case State.Wait:
-			if (Input.GetMouseButtonDown (0)) {
-				state = State.PlayerAttack;
-			}
-				break;
-
-			case State.PlayerAttack:
-			Debug.Log(turn + "ターン目");
-			turn++;
-			foreach (Player player in playerlist) {
-				foreach (Enemy enemy in enemylist) {
-					if(player.HP > 0){
-						if (enemy.HP > 0) {	//enemyが生きているかどうか
-							player.Attack (enemy);
-							break;
-						}
-					}
+				if (Input.GetMouseButtonDown(0))				{
+					state = State.PlayerAttack;
 				}
-			}
-			state = State.Playerwin;
-			foreach (Enemy enemy in enemylist) {
-				if(enemy.HP > 0){
+				break;
+			case State.PlayerAttack:
+				playerAttack();
+				if (enemyHP <= 0)
+				{
+					state = State.Finish;
+				}
+				else
+				{
 					state = State.EnemyAttack;
 				}
-			}
 				break;
-
 			case State.EnemyAttack:
-			foreach (Enemy enemy in enemylist) {
-				if(enemy.Turn == 0){
-					foreach (Player player in playerlist) {
-						if(enemy.HP > 0){
-							if (player.HP > 0) {
-								enemy.Attack (player);
-								break;
-							}
-						}
-					}
+				enemyAttack();
+				if (playerHP <= 0)
+				{
+					state = State.Finish;
 				}
-				else{
-					Debug.Log("=== "+enemy.NAME+" あと"+enemy.Turn+"ターンで攻撃 ===");
-					enemy.Turn--;
-				}
-			}
-			state = State.Playerlose;
-			foreach (Player player in playerlist) {
-				if(player.HP > 0){
+				else
+				{
 					state = State.Wait;
 				}
-			}
-			break;
-
-			case State.Playerwin:
-			Debug.Log ("=== Player Win! ===");
-			state = State.finish;
 				break;
-
-			case State.Playerlose:
-			Debug.Log ("=== Player Lose... ===");
-			state = State.finish;
-				break;
-
-			case State.finish:
+			case State.Finish:
+				if (!finishedFlg)
+				{
+					if (enemyHP <= 0)
+					{
+						Debug.Log("=== Player Win! ===");
+					}
+					else
+					{
+						Debug.Log("=== Player Lose... ===");
+					}
+					finishedFlg = true;
+				}
 				break;
 		}
+	}
+	void playerAttack()
+	{
+		int damage = UnityEngine.Random.Range(1, playerAttackPower + 1);
+		enemyHP -= damage;
+		Debug.Log("=== Player Attack! ===");
+		Debug.Log("Damage: " + damage);
+		Debug.Log("EnemyHP: " + enemyHP);
+	}
+	void enemyAttack()
+	{
+		int damage = UnityEngine.Random.Range(1, enemyAttackPower + 1);
+		playerHP -= damage;
+		Debug.Log("=== Enemy Attack! ===");
+		Debug.Log("Damage: " + damage);
+		Debug.Log("PlayerHP: " + playerHP);
 	}
 }
